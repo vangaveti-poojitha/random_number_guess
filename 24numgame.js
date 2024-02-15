c0f9abd3e0b76ed171cfdd8ca3e1945009294e62
vangaveti-poojitha <127885814+vangaveti-poojitha@users.noreply.github.com>
2024-02-15 22:45:45 +0530

    Add files via upload

---
+++ 24numgame.js
@@ -0,0 +1,15 @@
+const answer=Math.floor(Math.random()*10+1);
+let guesses=0;
+document.getElementById('submitbutton').onclick=function(){
+   let guess =document.getElementById('guessfield').value;
+   guesses+=1;
+   if(guess==answer){
+    alert(`${answer} is the number.it took you ${guesses} guesses to guess the answer`);
+   }
+   else if(guess<answer){
+    alert('too small');
+   }
+   else{
+    alert('too large');
+   }
+} 
